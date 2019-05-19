2e41cb1da960db02c3c34c72bf5a1186cb9461f8
Phillip <philliprognerud2@yahoo.com>
2019-05-19 12:50:25 -0700

    FUUUCCKKKK MEEE UPPP

--- authenticate.php
+++ authenticate.php
@@ -10,114 +10,101 @@
 </html>
 
 <?php
-	require_once 'login.php'; // login credentials for MySQL
-	require_once 'mysql_methods.php'; // programmer defined mysql methods to prevent hacking attempts
-	require_once 'verify_session.php'; // programmer defined methods to prevent session hijacking
+	require_once 'verify_session.php';
+	require_once 'login.php';
 
-	verify_session(basename(__FILE__)); // check the session
+	//Sanitize inputs
+	function sanitize($input){
+		return htmlentities(strip_tags(stripslashes($input)));
+	}
+
+	//Alert function
+	function alert($msg) {
+    echo "<script type='text/javascript'>alert('$msg');</script>";
+	}
+
+	verify_session(basename(__FILE__));
 
 	if (isset($_POST['email']) && isset($_POST['password'])) {
-
-		// connecting to a MySQL database
 		$conn = new mysqli($hn, $un, $pw, $db);
-		if ($conn->connect_error) mysql_fatal_error($conn->connect_error);
+		if ($conn->connect_error) mysqli_error($conn);
 
-		// prevent hacking attempts: SQL injections
-		$un = mysql_fix_string($conn, $_POST['email']);
-		$pw = mysql_fix_string($conn, $_POST['password']);
+		$un = sanitize($_POST['email']);
+		$pw = sanitize($_POST['password']);
 
-		// check if user exists in users table
-		if (user_in_users_tbl($conn, $un, $pw) === false)
-			user_in_admin_tbl($conn, $un, $pw);
-
-		$conn->close(); // close connection
+		if (users_table($conn, $un, $pw) === false) admin_table($conn, $un, $pw);
+		$conn->close();
 	}
 
-	function user_in_users_tbl($conn, $un, $pw)
-	{
-		// search if user exists in users table
-		//fname, password, salt1, salt2
-		if ($result = $conn->prepare("SELECT fname, password, salt1, salt2 FROM users NATURAL JOIN salt WHERE username=?;")) { // create a prepare statement
-			$result->bind_param('s', $un); // bind parameters for markers
-			$result->execute(); // execute query
-			$result->store_result(); // store result
-		}
-		else
-			mysql_fatal_error($conn->error);
+	function users_table($conn, $un, $pw){
+		if ($result = $conn->prepare("SELECT fname, password, salt1, salt2 FROM users NATURAL JOIN salt WHERE username=?;")) {
+			$result->bind_param('s', $un);
+			$result->execute();
+			$result->store_result();
+		}else{
+			mysqli_error($conn);
 
-		if ($result->num_rows == 1) { // user exists in users table
-			$result->bind_result($fn, $db_pw, $s1, $s2); // bind result variables
-			$result->fetch(); // fetch value
+			if ($result->num_rows == 1) {
+				$result->bind_result($fn, $db_pw, $s1, $s2);
+				$result->fetch();
 
-			// retrieve salt values
-			$salt1 = $s1;
-			$salt2 = $s2;
-			$token = hash('md5', "$salt1$pw$salt2");
+				$salt1 = $s1;
+				$salt2 = $s2;
+				$token = hash('md5', "$salt1$pw$salt2");
 
-			// verify user's password
-			if ($token === $db_pw) { // valid password
-				// starting a session
-				session_start();
-				// set $_SESSION to 1
-				$_SESSION['user'] = 1;
-				// preventing session hijacking
-				$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
-				$_SESSION['check'] = hash('md5', $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']);
-				echo '<script>
-						alert("Hi: '. $fn . ', you are now logged in as '. $un . '");
-						window.location = "infected_file.php";
-				      </script>';
+				if ($token === $db_pw) {
+					session_start();
+
+					$_SESSION['user'] = 1;
+					$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+					$_SESSION['check'] = hash('md5', $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']);
+
+					alert("Hi: '. $fn . ', you are now logged in as '. $un . '");
+					header("location: infected_file.php");
+				}else{
+					alert("Invalid username/password combination");
+					header("location: authenticate.php");
+				}
+			}else{
+				return false;
 			}
-			else // invalid password
-				echo '<script> alert("Invalid username/password combination"); window.location = "authenticate.php" </script>';
+			$result->close();
 		}
-		else // user not exist in users table
-			return false;
-
-		$result->close(); // close statement
 	}
 
-	function user_in_admin_tbl($conn, $un, $pw)
-	{
-		// search if user exists in users table
+	function admin_table($conn, $un, $pw){
+		if ($result = $conn->prepare("SELECT fname, password, salt1, salt2 FROM admin WHERE username=?;")) {
+			$result->bind_param('s', $un);
+			$result->execute();
+			$result->store_result()
+		}else{
+			mysqli_error($conn);
 
-		if ($result = $conn->prepare("SELECT fname, password, salt1, salt2 FROM admin WHERE username=?;")) { // create a prepare statement
-			$result->bind_param('s', $un); // bind parameters for markers
-			$result->execute(); // execute query
-			$result->store_result(); // store result
-		}
-		else
-			mysql_fatal_error($conn->error);
+			if ($result->num_rows == 1) {
+				$result->bind_result($fn, $db_pw, $s1, $s2);
+				$result->fetch();
+				$salt1 = $s1;
+				$salt2 = $s2;
+				$token = hash('md5', "$salt1$pw$salt2");
 
-		if ($result->num_rows == 1) { // user exists in admin table
-			$result->bind_result($fn, $db_pw, $s1, $s2); // bind result variables
-			$result->fetch(); // fetch value
+				if ($token === $db_pw) {
+					session_start();
+					$_SESSION['admin'] = 1;
+					$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+					$_SESSION['check'] = hash('md5', $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']);
+					alert("Hi, admin: '. $fn . ', you are now logged in as '. $un . '");
+					header("location: infected_file.php");
+				}else{
+					alert("Invalid username/password combination");
+					header("location: authenticate.php");
+				}
 
-			// retrieve salt values
-			$salt1 = $s1;
-			$salt2 = $s2;
-			$token = hash('md5', "$salt1$pw$salt2");
-
-			// verify user's password
-			if ($token === $db_pw) { // valid password
-				// starting a session
-				session_start();
-				// set $_SESSION to 1
-				$_SESSION['admin'] = 1;
-				// preventing session hijacking
-				$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
-				$_SESSION['check'] = hash('md5', $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']);
-				echo '<script>
-						alert("Hi, admin: '. $fn . ', you are now logged in as '. $un . '");
-						window.location = "infected_file.php";
-				      </script>';
+			}else{
+				alert("Invalid username/password combination");
+				header("location: authenticate.php");
 			}
-			else // invalid password
-				echo '<script> alert("Invalid username/password combination"); window.location = "authenticate.php" </script>';
-		}
-		else // user not exist in admin table
-			echo '<script> alert("Invalid username/password combination"); window.location = "authenticate.php" </script>';
 
-		$result->close(); // close statement
+			$result->close();
+		}
 	}
 ?>
