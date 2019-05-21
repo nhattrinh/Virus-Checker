9d2b03b695d2987868b45f84a8971a9b83a24f22
Nhat Trinh <goodyshoes_nt@yahoo.com>
2019-05-20 19:39:33 -0700

    stuff

--- session_verification.php
+++ session_verification.php
@@ -1,4 +1,22 @@
 <?php
+
+	function destroySessionAndData() {
+		$_SESSION = array();
+		setcookie(session_name(), '', time() - 2592000, '/');
+		session_destroy();
+		header('Location: index.php');
+	}
+
+	function isDifferentUser() {
+		if(isset($_SESSION['ip']) && isset($_SERVER['REMOTE_ADDR']) && isset($_SESSION['ua']) && isset($_SERVER['HTTP_USER_AGENT']) && 
+		   isset($_SESSION['check']) && isset($_SERVER['REMOTE_ADDR']) && isset($_SERVER['HTTP_USER_AGENT'])) {
+			if ($_SESSION['ip'] != $_SERVER['REMOTE_ADDR'] || $_SESSION['ua'] != $_SERVER['HTTP_USER_AGENT'] ||  
+				$_SESSION['check'] != hash('md5', $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']))
+				return true;
+		}
+		return false;
+	}
+	
 	function verifySession($currentPage) 
 	{
 		session_start();
@@ -15,22 +33,5 @@
 		} else {
 			session_destroy();
 		}
-	}
-
-	function isDifferentUser() {
-		if(isset($_SESSION['ip']) && isset($_SERVER['REMOTE_ADDR']) && isset($_SESSION['ua']) && isset($_SERVER['HTTP_USER_AGENT']) && 
-		   isset($_SESSION['check']) && isset($_SERVER['REMOTE_ADDR']) && isset($_SERVER['HTTP_USER_AGENT'])) {
-			if ($_SESSION['ip'] != $_SERVER['REMOTE_ADDR'] || $_SESSION['ua'] != $_SERVER['HTTP_USER_AGENT'] ||  
-				$_SESSION['check'] != hash('md5', $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']))
-				return true;
-		}
-		return false;
-	}
-
-	function destroySessionAndData() {
-		$_SESSION = array();
-		setcookie(session_name(), '', time() - 2592000, '/');
-		session_destroy();
-		header('Location: index.php');
 	}
 ?>
